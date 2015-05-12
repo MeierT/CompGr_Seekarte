8237616914f55275335ae82d909b0607f2b01946
meiert <meiert@hochschule-trier.de>
2015-05-12 16:14:31 +0200

    add frame

---
+++ src/java/de/hochschuleTrier/seekarte/swingComponents/SeeKarteFrame.java
@@ -0,0 +1,18 @@
+package de.hochschuleTrier.seekarte.swingComponents;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class SeeKarteFrame extends JFrame
+{
+    public SeeKarteFrame()
+    {
+        /* SIZE */
+        this.setSize(300, 300);
+        
+        
+        
+        /* VISIBLE */
+        this.setVisible(true);
+    }
+}
